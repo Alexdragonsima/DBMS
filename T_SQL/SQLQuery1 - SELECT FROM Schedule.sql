8ec023497780ffc52bd3a91411505b2cb8a7860b
Алексей Семионов <alehaaaaa@mail.ru>
2025-02-24 15:35:54 +0300

    SELECT/INSERT Schedule DONE

---
+++ T_SQL/SQLQuery1 - SELECT FROM Schedule.sql
@@ -0,0 +1,20 @@
+USE DBMS_DDL;
+GO
+
+SET DATEFIRST 1;
+
+--DELETE FROM Schedule;
+
+SELECT
+		[Группа]		=	group_name,
+		[Дисциплина]	=	discipline,
+		[Преподаватель]	=	FORMATMESSAGE(N'%s %s %s', last_name,first_name,middle_name),
+		[День]			=	DATENAME(WEEKDAY, [date]),
+		[Дата]			=	[date],
+		[Время]			=	[time],
+		[Статус]		=	IIF(spent=1,N'Проведено',N'Запланировано')
+FROM	Schedule, Groups, Disciplines, Teachers
+WHERE	[group]			=	group_id
+AND		discipline		=	discipline_id
+AND		teacher			=	teacher_id
+;
