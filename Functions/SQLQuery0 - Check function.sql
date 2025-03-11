2410d01fbc3de791068c8b19893fb9fff6f915ab
Алексей Семионов <alehaaaaa@mail.ru>
2025-03-11 15:53:45 +0300

    Summertime sadness

--- Functions/SQLQuery0 - Check function.sql
+++ Functions/SQLQuery0 - Check function.sql
@@ -2,7 +2,14 @@ USE DBMS_DDL;
 SET DATEFIRST 1;
 GO
 
-PRINT(dbo.GetGroupID(N'PV_319'));
-PRINT(dbo.GetDisciplineID(N'Hardware%'));
---EXEC sp_PrintScheduleForGroup N'PV_319';
-PRINT (dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
+--PRINT(dbo.GetGroupID(N'PV_319'));
+--PRINT(dbo.GetDisciplineID(N'Hardware%'));
+----EXEC sp_PrintScheduleForGroup N'PV_319';
+--PRINT(dbo.CountSpentLessons(N'PV_319', N'??????????? ????????????????%'));
+--PRINT(dbo.GetNewYearHolidaysStartDate(2023));
+PRINT(dbo.GetSummerHolidaysStart(2023));
+--PRINT(dbo.GetHolidaysDuration(N'??????%'));
+
+--PRINT('---------------------------');
+--PRINT(dbo.GetHolidayStartDate(N'??????%', 2025));
+--PRINT(dbo.GetEasterDate(2004));
